67c9222768557e3ea289f977c7ce787d913d7d0a
Dobroslav Tokoš <dobroslav@tokos.sk>
2024-05-17 14:27:07 +0200

    Updated build tools config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import {defineConfig} from 'vite';
 import {svelte} from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [svelte()],
+	resolve: {
+		alias: {
+			'@gitlab': path.resolve(__dirname, './src/services/gitlab'),
+		},
+	},
 	build: {
 		outDir: 'dist',
 		sourcemap: true,
