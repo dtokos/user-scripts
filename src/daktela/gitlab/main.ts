db613be0eac9109534e13c237173d04b876c4a20
Dobroslav Tokoš <dobroslav@tokos.sk>
2024-11-02 14:13:47 +0100

    Removed unused stuff

--- src/daktela/gitlab/main.ts
+++ src/daktela/gitlab/main.ts
@@ -1,6 +1,4 @@
 import './styles.css';
-// import injectCopyCommitAsComment from './features/copy-commit-as-comment/inject';
 import { inject } from './features/inject';
 
-// injectCopyCommitAsComment();
 inject();
--- src/services/gitlab/ui/containers.ts
+++
@@ -1,9 +0,0 @@
-export const containers = {
-	justifyBetween(...children: HTMLElement[]): HTMLDivElement {
-		const container = document.createElement('div');
-		container.classList.add('gl-display-flex', 'gl-justify-content-space-between');
-		children.forEach(child => container.appendChild(child));
-
-		return container;
-	},
-} as const;
